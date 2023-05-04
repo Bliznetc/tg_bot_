b2e891f428c64e6b0a106df19a1a757ede3a4bfa
Igor Moroz <bliznetc2006@gmail.com>
2023-05-04 22:00:28 +0300

    убрал повторения в словаре

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton 
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 import constants as const
 import random
 import dictionary as dt
@@ -7,6 +7,7 @@ from telebot import types
 
 # Initialize the bot using the bot token
 bot = telebot.TeleBot(f"{const.API_KEY}")
+
 
 # Define a function to handle the /start command
 @bot.message_handler(commands=['start'])
@@ -19,34 +20,40 @@ def start_handler(message):
 # Define a function to handle the /help command
 @bot.message_handler(commands=['help'])
 def help_handler(message):
-    bot.reply_to(message, 'Type\n"/quiz" - to get a quiz\n "/add_word" - to add a new word\n "/whole_dict" - to check all the words')
+    bot.reply_to(message, 'Type\n"/quiz" - to get a quiz\n "/add_word" - to add a new word\n "/whole_dict" - to check '
+                          'all the words')
+
 
 # Define a function to handle the /echo command
 @bot.message_handler(commands=['echo'])
 def echo_handler(message):
-    message_text = message.text.split(maxsplit=1)[1] # Get the text after the command
+    message_text = message.text.split(maxsplit=1)[1]  # Get the text after the command
     bot.reply_to(message, message_text)
+
 
 @bot.message_handler(commands=['info'])
 def info_handler(message):
     bot.reply_to(message, 'Rostislav Budarin - Lepshy')
+
 
 @bot.message_handler(commands=['add_word'])
 def add_word(message):
     bot.reply_to(message, 'Введите новое слово и перевод в формате "слово-перевод"')
     bot.register_next_step_handler(message, add_word_to_dict)
 
+
 def add_word_to_dict(message):
     new_key = message.text.split('-')[0]
     new_meaning = message.text.split('-')[1]
-    dt.quiz_list.append({'word': new_key, 'translation': new_meaning}) #здесь было неправильно
+    dt.quiz_list.append({'word': new_key, 'translation': new_meaning})  # здесь было неправильно
     bot.send_message(message.chat.id, f"ваше слово: {new_key}, перевод: {new_meaning}")
     print(len(dt.quiz_list))
     printAllWords(message.from_user.id)
 
+
 def printAllWords(message_id):
     for word in dt.quiz_list:
-       bot.send_message(message_id, f"{word['word']} - {word['translation']}")
+        bot.send_message(message_id, f"{word['word']} - {word['translation']}")
 
 
 # generates quiz when user types "/quiz"
@@ -55,7 +62,7 @@ def generate_quiz():
     answer_options = random.sample(dt.quiz_list, 3)
     f = 1
     for a in answer_options:
-        if (a['word'] == word['word']):
+        if a['word'] == word['word']:
             f = 0
 
     if f == 1:
@@ -71,9 +78,10 @@ def generate_quiz():
                 answer_options.append(a)
                 break
 
-    print(answer_options) # for debugging
+    print(answer_options)  # for debugging
     random.shuffle(answer_options)
     return word, answer_options
+
 
 @bot.message_handler(commands=['quiz'])
 def send_quiz(message):
@@ -81,8 +89,10 @@ def send_quiz(message):
     quiz_text = f"What is the Russian translation of the word '{word['word']}'?\n\n"
     quiz_keyboard = types.InlineKeyboardMarkup()
     for answer_option in answer_options:
-        quiz_keyboard.add(types.InlineKeyboardButton(answer_option['translation'], callback_data=str(answer_option == word)))
+        quiz_keyboard.add(
+            types.InlineKeyboardButton(answer_option['translation'], callback_data=str(answer_option == word)))
     bot.send_message(chat_id=message.chat.id, text=quiz_text, reply_markup=quiz_keyboard)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def check_quiz(call):
@@ -99,5 +109,5 @@ print(__name__)
 if __name__ == '__main__':
     bot.polling()
 
-#сделал фигню
-#сделал фигню №2
+# сделал фигню
+# сделал фигню №2

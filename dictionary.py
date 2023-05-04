89a57d2c29c9dfba61ca7c9a918f5279b44c6424
Igor Moroz <bliznetc2006@gmail.com>
2023-05-04 21:28:15 +0300

    убрал повторения в словаре

--- dictionary.py
+++ dictionary.py
@@ -2,5 +2,6 @@ quiz_list = [
     {'word': 'cat', 'translation': 'кот'},
     {'word': 'dog', 'translation': 'собака'},
     {'word': 'apple', 'translation': 'яблоко'},
+    {'word': 'man', 'translation': 'мужчина'},
     # add more words and translations to the list
 ]

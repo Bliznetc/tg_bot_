4470c2dfc7983849b96d41d3be9b0f0b7840034d
Desperadosol <igor.homichenko123@gmail.com>
2023-05-06 01:55:53 +0200

    updated API keys

--- constants.py
+++ constants.py
@@ -1,1 +1,3 @@
-API_KEY = "6229703706:AAGmUdzEuYC9vyPLiOh8Kpg112NtmA3EWf0"
+API_KEY_HOSTED = "6229703706:AAGmUdzEuYC9vyPLiOh8Kpg112NtmA3EWf0" #this is the api key of the hosted bot
+API_KEY_TEST = "6260512391:AAECSUgkmd5LvJy2SUx9kFN_5KG5SEpYvPU" #this is the api key of the test bot, the bot we can experiment with
+# master will always run on the API_KEY_HOSTED, while we can test our code on the test bot with the API_KEY_TEST

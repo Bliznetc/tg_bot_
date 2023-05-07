840a71cdc4a5eecf35c5a501b9549ff73868405e
Igor Moroz <bliznetc2006@gmail.com>
2023-05-07 16:29:43 +0300

    I got you What do we have? Right, your mom's ass. And a bot sending quizes every 30-10-15-1000-FUCK seconds

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-import constants as const
 import random
-import dictionary as dt
+import threading
+
+import telebot
 from telebot import types
-import datetime
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+import constants as const
+import dictionary as dt
 
 # Initialize the bot using the bot token
 bot = telebot.TeleBot(f"{const.API_KEY_TEST}")
@@ -54,7 +56,7 @@ def add_word_to_dict(message):
 
 def printAllWords(message_id):
     for word in dt.quiz_list:
-        bot.send_message(message_id, f"{word['word']} - {word['translation']}") #зачем????
+        bot.send_message(message_id, f"{word['word']} - {word['translation']}")  # зачем????
 
 
 # generates quiz when user types "/quiz"
@@ -84,7 +86,23 @@ def send_quiz_via_chatid(chat_id):
     for answer_option in answer_options:
         quiz_keyboard.add(
             types.InlineKeyboardButton(answer_option['translation'], callback_data=str(answer_option == word)))
-    bot.send_message(chat_id=chat_id, text=quiz_text, reply_markup=quiz_keyboard) #здесь была ошибка, так как функция принимала именные аргументы, а ты передал позиционный
+    bot.send_message(chat_id=chat_id, text=quiz_text,
+                     reply_markup=quiz_keyboard)  # здесь была ошибка, так как функция принимала именные аргументы,
+                                                    # а ты передал позиционный
+
+
+def send_quiz_spam():
+    word, answer_options = generate_quiz()
+    quiz_text = f"What is the Russian translation of the word '{word['word']}'?\n\n"
+    quiz_keyboard = types.InlineKeyboardMarkup()
+    for chat_id in const.chat_ids:
+        for answer_option in answer_options:
+            quiz_keyboard.add(
+                types.InlineKeyboardButton(answer_option['translation'], callback_data=str(answer_option == word)))
+        bot.send_message(chat_id=chat_id, text=quiz_text,
+                         reply_markup=quiz_keyboard)
+
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def check_quiz(call):
@@ -96,24 +114,32 @@ def check_quiz(call):
     bot.answer_callback_query(callback_query_id=call.id, text=message_text)
 
 
+def set_interval(func, sec):
+    print("Я вызвал set_interval")
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+
+set_interval(send_quiz_spam, 28)
+
 print(__name__)
 
 if __name__ == '__main__':
     bot.polling()
-    now = datetime.datetime.now()
-    cur_minute = now.minute
-    cur_second = now.second
-    cur_second = 20 # изменил это на 20б чтобы выполнялся код ниже
-    print("cur_second - " + str(cur_second))# fixed string concatenation
-    if cur_second == 20:
-        send_quiz_via_chatid(const.chat_ids[0])# выдаёт ошибку: чат не найден, так как я изменил api key на тестовый, поэтому нужно будет изменить constants.py
 
-# этот код отправит тебе один квиз после того, когда ты остановишь бота. Надо смотреть как это сделать дальше. 
+# Этот код отправит тебе один квиз после того, когда ты остановишь бота. Надо смотреть как это сделать дальше.
 # Пока что не изменил dictionary.py на dictionary.json, потому что заебался и хочу спать)))
 
-# Кстати этот сервис который нам хостит бота, также может хостить БД, что можно использовать для хранения id чатов и отдельных словарей для пользователей
+# Кстати этот сервис который на м хостит бота, также может хостить БД, что можно использовать для хранения id чатов и
+# отдельных словарей для пользователей
 
-# B последнее, было бы неплохо изменить /add_word, чтобы он можно было закидывать много слов (например "слово1-перевод1;слово2-перевод2;..."), а также добавить 
+# B последнее, было бы неплохо изменить /add_word, чтобы он можно было закидывать много слов (например "слово1-перевод1;
+# слово2-перевод2;..."), а также добавить
 # возможность закидывать файл
 
 # постараюсь добавить всё в issues, чтобы оно просто висело, а то заметки в коде - это пиздец

4c3ddb8f51bc2cdc62400db7bf3855e6890283a2
Igor Moroz <bliznetc2006@gmail.com>
2023-05-11 12:09:29 +0300

    improved add_word

--- json_functions.py
+++ json_functions.py
@@ -8,16 +8,18 @@ def create_answer_options():
     return random.sample(dictionary, 4)
 
 def add_word_to_dt(message):
-    new_key = message.text.split('-')[0]
-    new_meaning = message.text.split('-')[1]
+    arr = message.text.split(';')
+    for x in arr:
+        new_key = x.split('-')[0]
+        new_meaning = x.split('-')[1]
 
-    with open("dictionary.json", "r", encoding="utf-8") as file:
-        dictionary = json.load(file)
+        with open("dictionary.json", "r", encoding="utf-8") as file:
+            dictionary = json.load(file)
 
-    dictionary.append({'word': new_key, 'translation': new_meaning})
+        dictionary.append({'word': new_key, 'translation': new_meaning})
 
-    with open("dictionary.json", "w") as file:
-        json.dump(dictionary, file, indent=4)
+        with open("dictionary.json", "w") as file:
+            json.dump(dictionary, file, indent=4)
     
     
 

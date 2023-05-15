37b6bbf93199672eae1bc82bea0e30ffe2cd89b1
Igor Moroz <bliznetc2006@gmail.com>
2023-05-15 16:08:29 +0300

    что-то попытался сделать с файлами, но сломал хуйню

--- json_functions.py
+++ json_functions.py
@@ -1,14 +1,16 @@
 import json
 import random
 
+
 def create_answer_options():
     with open("dictionary.json", "r", encoding="utf-8") as file:
         dictionary = json.load(file)
-    
+
     return random.sample(dictionary, 4)
 
-def add_word_to_dt(message):
-    arr = message.text.split(';')
+
+def add_word_to_dt(cur_text):
+    arr = cur_text.split(';')
     for x in arr:
         new_key = x.split('-')[0]
         new_meaning = x.split('-')[1]
@@ -20,6 +22,3 @@ def add_word_to_dt(message):
 
         with open("dictionary.json", "w") as file:
             json.dump(dictionary, file, indent=4)
-    
-    
-

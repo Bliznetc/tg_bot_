1ec1245315f8647581f66b1e36def8909c25cc2c
Desperadosol <igor.homichenko123@gmail.com>
2023-05-18 11:21:54 +0200

    почистил

--- json_functions.py
+++ json_functions.py
@@ -10,21 +10,19 @@ def create_answer_options():
 
 
 def add_word_to_dt(cur_text):
+
+    with open("dictionary.json", "r", encoding="utf-8") as file:
+            dictionary = json.load(file)
+
     arr = cur_text.split(';')
     for x in arr:
         if x.find('-') == -1:
-            continue;
+            continue
 
-        new_key = x.split('-')[0]
-        new_meaning = x.split('-')[1]
+        new_key, new_meaning = x.split('-')
 
         new_key = new_key.replace('\n', '')
         new_key = new_key.replace(' ', '')
-        new_key = new_key.lower()
-
-        with open("dictionary.json", "r", encoding="utf-8") as file:
-            dictionary = json.load(file)
-
         new_key = new_key.lower()
 
         f = 1
@@ -38,6 +36,7 @@ def add_word_to_dt(cur_text):
         if f == 1:
             print("Добавил слово")
             dictionary.append({'word': new_key, 'translation': new_meaning})
-            with open("dictionary.json", "w") as file: #this code was outside of for
-                json.dump(dictionary, file, indent=4)
+
+    with open("dictionary.json", "w") as file: #this code was outside of for
+        json.dump(dictionary, file, indent=4)
 

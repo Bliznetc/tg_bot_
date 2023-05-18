479999d94d6bb7b94f59f6f3e8727d2f8aa1f0ff
Desperadosol <igor.homichenko123@gmail.com>
2023-05-18 18:39:23 +0200

    example of lambda for garik

--- test_func_for_garik.py
+++ test_func_for_garik.py
@@ -10,7 +10,7 @@ with open("dictionary.json", "r", encoding="utf-8") as file:
     dictionary = json.load(file)
 
 
-dictionary.sort(key=myFuncSort)
+dictionary.sort(key=lambda x: x['degree'], reverse=True) #для примера
 for i in range(10):
     print(dictionary[i])
 

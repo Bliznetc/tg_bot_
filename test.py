9d2f06da2a5b681d3be23431e87cc64b94d791f1
Desperadosol <igor.homichenko123@gmail.com>
2023-05-19 23:17:24 +0200

    почистил

--- test.py
+++ test.py
@@ -1,31 +1,5 @@
-import json
-import constants as const
-import json_functions as jsonFunc
-import random
+print("Удалил всё ненужное")
 
-"""
-def registration(user_id):
-    if user_id in const.chat_ids:
-        pass
-    else:
-        with open(f"user_dictionaries/{user_id}.json", "w") as file:
-            json.dump([], file, indent=4)
-        
-if __name__ == "__main__":
-    
-    with open("user_ids.json", "r") as file:
-        user_ids = json.load(file)
-
-    for user_id in user_ids:
-        registration(user_id)
-"""
-
-s = "abab"
-l = list(s);
-l[0] = s[0].upper()
-s = "".join(l)
-print(s)
-
-#в этом файле я создал наши словари, которые соответсвуют нашим id
-#этот файл не готов к использованию, и будет полностью переделан
-
+#удаление словаря
+with open("dictionary.json", "w") as file:
+    json.dump([], file)

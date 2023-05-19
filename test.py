966505c7fb013452936c83c631e481a1f7d70146
Desperadosol <igor.homichenko123@gmail.com>
2023-05-19 02:15:20 +0200

    грязюка

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 import json
+import constants as const
 
+def registration(user_id):
+    if user_id in const.chat_ids:
+        pass
+    else:
+        with open(f"user_dictionaries/{user_id}.json", "w") as file:
+            json.dump([], file, indent=4)
+        
+if __name__ == "__main__":
+    
+    with open("user_ids.json", "r") as file:
+        user_ids = json.load(file)
 
+    for user_id in user_ids:
+        registration(user_id)
+
+#в этом файле я создал наши словари, которые соответсвуют нашим id
+#этот файл не готов к использованию, и будет полностью переделан

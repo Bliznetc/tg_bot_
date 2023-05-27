eff40f95787605ca81cfd4ad75017a04cdbfe4eb
Desperadosol <igor.homichenko123@gmail.com>
2023-05-27 03:48:41 +0200

    не трогать db_..._test.py, ускорил на 0.3-0.6сек

--- db_interface_test.py
+++ db_interface_test.py
@@ -2,82 +2,86 @@ import json
 import mysql.connector
 import datetime
 from datetime import datetime
+
+import asyncio
 import time
 
-def connect_database():
-    connection = mysql.connector.connect(
+t = time.time()
+try:
+    connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+        pool_name="my_pool",
+        pool_size=5,
+        pool_reset_session=True,
         host="containers-us-west-112.railway.app",
         user="root",
         password="QS8qQZtt5Ey4XhAcrhkz",
         database="railway",
         port="6633"
-    )
-    return connection
-
+        )
+except mysql.connector.Error as e:
+    print("Error while creating connection pool:", e)
+print(time.time()-t, "takes to set up the connection")
 
 # Добавляет пользователя в database
 def store(user_id: int, access: str, mailing: bool):
     t = time.time()
-    connection = connect_database()
+
+    connection = connection_pool.get_connection()
     cursor = connection.cursor()
 
-    # new_block
     query_check = "SELECT user_id FROM User_Dictionaries"
     cursor.execute(query_check)
     data = [i[0] for i in cursor.fetchall()]
-    # print(data)
 
     if user_id in data:
-        cursor.close()
-        connection.close()
-        print(time.time()-t, "регестрация пользователя")
-        return "Вы уже зарегестрированы"
+        text = "Вы уже зарегестрированы"
     else:
         json_data = json.dumps([])
-
         print(user_id, access, json_data, mailing)
         # Insert user data into the database
         query = "INSERT INTO User_Dictionaries (user_id, access, dictionary, Mailing) VALUES (%s, %s, %s, %s)"
         cursor.execute(query, (user_id, access, json_data, mailing))
-
         # Commit the changes and close the connection
         connection.commit()
-        cursor.close()
-        connection.close()
-        print(time.time()-t, "регестрация пользователя")
-        return "Добро пожаловать!"
+        text = "Добро пожаловать!"
+
+    cursor.close()
+    connection.close()
+    print(time.time()-t, "регестрация пользователя")
+    return text
 
 
+    
 # Получает все слова
-def get_words():
+async def get_words():
     t = time.time()
-    connection = connect_database()
-    print(time.time()-t, "old connect")
+    
+    connection = connection_pool.get_connection()
     cursor = connection.cursor()
-    print(time.time()-t, "old cursor")
+    
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, connection.close)
+
     # Выполнение SQL-запроса
     query = "SELECT dictionary FROM User_Dictionaries WHERE user_id = 955008318"
     cursor.execute(query)
-    print(time.time()-t, "old query")
+    
     # Получение результатов
     cur_dictionary_json = cursor.fetchall()
     data = cur_dictionary_json[0][0]
     dictionary = json.loads(data)
-    print(time.time()-t, "old data")
 
     cursor.close()
-    print(time.time()-t, "old cursor")
-    connection.close()
+    
+    print(time.time()-t, "получение слов")
 
-    print(time.time()-t, "old")
     return dictionary
 
 
 # Добавляет список слов в database
 def add_word_to_bd(arr: list, user_id: int):
-    start_time = datetime.datetime.now()
 
-    connection = connect_database()
+    connection = connection_pool.get_connection()
     cursor = connection.cursor()
 
     new_dict = [{"word": item[0], "degree": 0, "translation": item[1]} for item in arr]
@@ -103,14 +107,10 @@ def add_word_to_bd(arr: list, user_id: int):
     cursor.close()
     connection.close()
 
-    end_time = datetime.datetime.now()
-    execution_time = end_time - start_time
-    print("время выполнения ", execution_time.seconds)
-
 
 # Возвращает значение Mailing
 def started_mailing(current_user_id):
-    connection = connect_database()
+    connection = connection_pool.get_connection()
     cursor = connection.cursor()
 
     query = f"SELECT Mailing FROM User_Dictionaries WHERE user_id = {current_user_id}"
@@ -123,7 +123,7 @@ def started_mailing(current_user_id):
 
 # обновляет значение Mailing
 def update_mailing(current_user_id, new_value):
-    connection = connect_database()
+    connection = connection_pool.get_connection()
     cursor = connection.cursor()
 
     # Выполнение SQL-запроса
@@ -142,7 +142,7 @@ def update_mailing(current_user_id, new_value):
 # Добавил проверку прямо в store()
 # Проверяет, существует ли user с указанным id
 def check_user_exists(user_id):
-    connection = connect_database()
+    connection = connection_pool.get_connection()
     cursor = connection.cursor()
 
     # Выполнение SQL-запроса
@@ -162,7 +162,7 @@ def check_user_exists(user_id):
 
 # Возвращает список всех юзеров, которым нужно отправить квиз в текущий момент
 def get_needed_users():
-    connection = connect_database()
+    connection = connection_pool.get_connection()
     cursor = connection.cursor()
 
     query = f"SELECT user_id, sent_time, Mailing FROM User_Dictionaries WHERE Mailing != 0"

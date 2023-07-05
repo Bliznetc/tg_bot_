569e5af82fc3aa5f64055945d55a01e8e1905c9a
Igor Moroz <bliznetc2006@gmail.com>
2023-07-05 18:59:58 +0300

    MINOR Change

--- test.py
+++ test.py
@@ -68,43 +68,43 @@ import db_interface
 #         except Exception as e:
 #             print(e)
 #     return wrapper
-
-import db_interface
-import re
-import codecs
-
-
-def formating(dict_id):
-    content = db_interface.get_words_by_dict_id(dict_id)
-
-    for key, value in content.items():
-        for elem in value:
-            for key2, value2 in elem.items():
-                if isinstance(value2, str):
-                    elem[key2] = transform(value2)
-
-    db_interface.update_content(dict_id, content)
-
-    print("probitie")
-
-
-def check_substring(s):
-    match = re.search(r'u0\w{3}', s)
-    if match:
-        return (match.start(), match.group())
-    return -1
-
-
-def transform(string: str):
-    while check_substring(string) != -1:
-        index, letter = check_substring(string)
-        print(index, letter)
-        string = string[:index] + codecs.decode(f"\\{letter}", "unicode_escape") + string[index + 5:]
-        print(string)
-    print('----------------------------------------------')
-    return string
-
-
-formating('TEST_B1')
+#
+# import db_interface
+# import re
+# import codecs
+#
+#
+# def formating(dict_id):
+#     content = db_interface.get_words_by_dict_id(dict_id)
+#
+#     for key, value in content.items():
+#         for elem in value:
+#             for key2, value2 in elem.items():
+#                 if isinstance(value2, str):
+#                     elem[key2] = transform(value2)
+#
+#     db_interface.update_content(dict_id, content)
+#
+#     print("probitie")
+#
+#
+# def check_substring(s):
+#     match = re.search(r'u0\w{3}', s)
+#     if match:
+#         return (match.start(), match.group())
+#     return -1
+#
+#
+# def transform(string: str):
+#     while check_substring(string) != -1:
+#         index, letter = check_substring(string)
+#         print(index, letter)
+#         string = string[:index] + codecs.decode(f"\\{letter}", "unicode_escape") + string[index + 5:]
+#         print(string)
+#     print('----------------------------------------------')
+#     return string
+#
+#
+# formating('TEST_B1')
 
 
